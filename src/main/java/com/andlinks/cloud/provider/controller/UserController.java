e08f74763af2bb837e32e4a7934a3cb03882ddfc
chencong <1085576236@qq.com>
2018-07-04 18:29:39 +0800

    feign接口demo完善

--- src/main/java/com/andlinks/cloud/provider/controller/UserController.java
+++ src/main/java/com/andlinks/cloud/provider/controller/UserController.java
@@ -3,18 +3,36 @@ package com.andlinks.cloud.provider.controller;
 import com.andlinks.cloud.provider.entity.User;
 import com.andlinks.cloud.provider.reposity.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 public class UserController {
-  @Autowired
-  private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-  @GetMapping("/{id}")
-  public User findById(@PathVariable Long id) {
-    User findOne = this.userRepository.getOne(id);
-    return findOne;
-  }
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
+        User findOne = this.userRepository.getOne(id);
+        return findOne;
+    }
+
+    @GetMapping("/getUser")
+    public User findByIdAndUsername(@RequestParam Long id,@RequestParam String username ){
+        User user=userRepository.findByIdAndUsername(id,username);
+        return  user;
+    }
+
+    @GetMapping("getByMap")
+    public User findByIdAndUsername(@RequestParam Map<String,String> map ){
+        User user=userRepository.findByIdAndUsername(Long.valueOf(map.get("id")),map.get("username"));
+        return  user;
+    }
+
+    @PostMapping("/postUser")
+    public User postUser(@RequestBody User user){
+        return user;
+    }
 }
--- src/main/java/com/andlinks/cloud/provider/reposity/UserRepository.java
+++ src/main/java/com/andlinks/cloud/provider/reposity/UserRepository.java
@@ -2,8 +2,10 @@ package com.andlinks.cloud.provider.reposity;
 
 import com.andlinks.cloud.provider.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByIdAndUsername(Long id, String username);
 }
